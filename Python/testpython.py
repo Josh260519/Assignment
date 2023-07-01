0b2af2fb4b66565fa5a0e89ae0d4882886678f0d
Josh260519 <129236841+Josh260519@users.noreply.github.com>
2023-07-01 23:14:56 +0800

    Update testpython.py

--- Python/testpython.py
+++ Python/testpython.py
@@ -1,11 +1,10 @@
 import socket
 import threading
-from queue import Queue
+import datetime
 
-# Define the host and port to listen on
-host = '192.168.17.128'  # Change this to your desired host
-port = 8484  # Change this to your desired port
-max_connections = 5  # Maximum number of connections in the pool
+# Defining host and port to listen for connections
+host = '192.168.17.128'  # IP Address of host
+port = 8484  # Port to listen to
 
 def handle_client(client_socket, client_address):
     # Receive data from the client
@@ -15,55 +14,40 @@ def handle_client(client_socket, client_address):
             break
         print("Received data from {}: {}: {}".format(client_address[0], client_address[1], data))
 
-        # Echo the received data back to the client
-        client_socket.sendall(data.encode('utf-8'))
+        # Combine the client's response with the current date and time
+        current_datetime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        response = "[{}] {}".format(current_datetime, data)
+
+        # Echo the modified response back to the client
+        client_socket.sendall(response.encode('utf-8'))
 
     # Close the client socket
     client_socket.close()
     print("Connection closed with {}:{}".format(client_address[0], client_address[1]))
 
-def worker(connection_pool):
-    while True:
-        # Get a client connection from the connection pool
-        client_socket, client_address = connection_pool.get()
-
-        # Handle the client connection
-        handle_client(client_socket, client_address)
-
-        # Mark the task as done for the connection pool
-        connection_pool.task_done()
-
 def main():
     # Create a socket object
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # Set the SO_REUSEADDR option
+    # Enabling SO_REUSEADDR option
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
     # Bind the socket to the host and port
     sock.bind((host, port))
 
     # Listen for incoming connections
-    sock.listen(max_connections)
+    sock.listen(5)
 
     print("Server is listening on {}:{}".format(host, port))
-
-    # Create a connection pool as a thread-safe queue
-    connection_pool = Queue(max_connections)
-
-    # Create worker threads
-    for _ in range(max_connections):
-        t = threading.Thread(target=worker, args=(connection_pool,))
-        t.daemon = True
-        t.start()
 
     while True:
         # Accept a connection from a client
         client_socket, client_address = sock.accept()
         print("Connected to {}:{}".format(client_address[0], client_address[1]))
 
-        # Put the client connection in the connection pool
-        connection_pool.put((client_socket, client_address))
+        # Creates a thread to handle the client
+        client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
+        client_thread.start()
 
 if __name__ == '__main__':
     main()
